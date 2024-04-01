1b2e1f46c49f935976189e104e94b7bf55c965f1
coelho1988 <138686366+coelho1988@users.noreply.github.com>
2024-04-01 18:04:14 -0300

    V.2
    Segunda parte do desafio

--- banco.py
+++ banco.py
@@ -1,40 +1,60 @@
-print('''Escolha a operação
+
+print('''
+Escolha a operação
 [1]Saque
 [2]Deposito
 [3]Extrato
-[4]Sair''' )
+[0]Sair''' )
 
 operacao = int(input())
-saldo = 0
+saldo = 500
 extrato = []
 limitediario = 3
 
+def deposito (valor, saldo, extrato, /):
+    valor = float(input('digite quanto quer depositar: '))
+    saldo += valor
+    extrato.append(f"deposito: {valor}")     
+    print('R${:.2f} depositado com sucesso'.format(valor))
+    return saldo, extrato
+
+def saque (*,valor, saldo, extrato):        
+    global limitediario
+    if limitediario <= 0:
+        print('Limites de saques atingido') 
+    else:
+        valor = float(input('Digite quanto quer sacar: '))
+        if saldo < valor:
+            print('Saldo insuficiente')
+        elif valor > 500:
+            print('Valor para saque invalido')
+        else:
+            saldo -= valor
+            extrato.append(f"saque: {valor}")        
+            print('R${:.2f} sacado com sucesso'.format(valor))
+            limitediario -= 1
+            print(limitediario)        
+    return saldo, extrato,        
+            
+def exibir_extrato (saldo, /, *, extrato):
+    print('='*5, 'EXTRATO', '='*5)
+    print(*extrato, sep= '\n')
+    print('='*19)
+    print('saldo:R${:.2f}'.format(saldo))
+    return saldo, extrato
+            
 while True:
        
-    if operacao == 1:
-        if limitediario > 0:
-            saque = float(input('Digite quanto quer sacar: '))
-            if saldo > saque and saque <= 500:
-                saldo -= saque
-                extrato.append(f"saque: {saque}")
-                limitediario -= 1
-                print('R${:.2f} sacado com sucesso'.format(saque))
-            else:
-                print('Valor invalido ou saldo insuficiente')
-        else:
-            print('Limite de Saques diarios execidido')
-            
-    elif operacao == 2:
-        deposito = float(input('digite quanto quer depositar: '))
-        saldo += deposito
-        extrato.append(f"deposito: {deposito}")     
-        print('R${:.2f} depositado com sucesso'.format(deposito))
+    if operacao == 1: #saque        
+            saque(valor=0, saldo=saldo, extrato=extrato)            
+                    
+    elif operacao == 2: #Deposito 
+        saldo, extrato = deposito(0, saldo, extrato)
 
-    elif operacao == 3:
-        print('saldo:R${:.2f}',saldo)
-        print(*extrato, sep= '\n')
+    elif operacao == 3: #Extrato
+        exibir_extrato(saldo, extrato=extrato)
 
-    elif operacao == 4:
+    elif operacao == 0:
         print('Operação finalizada')
         break
         
